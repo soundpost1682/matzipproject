bb8d3d2eda8e6ed644a6a4c665d6351994648d98
soundpost1682 <guarneri1682@gmail.com>
2025-10-24 14:23:12 -0700

    icons setup and apply

--- src/navigations/DrawerNavigation.tsx
+++ src/navigations/DrawerNavigation.tsx
@@ -8,9 +8,33 @@ import { FeedStack } from "./FeedNavigation";
 import DrawerButton from "../components/DrawerButton";
 import { colors } from "../constants/colors";
 import CustomDrawerContent from "../components/CustomDrawerContent";
+import { MainDrawerParamList } from "../types/navigation";
+import FontAwesome6 from "@react-native-vector-icons/fontawesome6";
+type DrawerIconName = 'map'|'book'|'calendar'
+
+function DrawerIcons(routeName:keyof MainDrawerParamList, focused:boolean){
+    let iconName : DrawerIconName;
+
+    switch(routeName){
+        case 'Map':{
+            iconName = 'map'
+            break;
+        }
+        case 'Feed':{
+            iconName = 'book'
+            break
+        }
+        case 'Calendar':{
+            iconName = 'calendar'
+            break
+        }
+    }
+    return <FontAwesome6 name={iconName} iconStyle='solid' size={20} color={focused?colors.WHITE : colors.GRAY_300}/>
+}
 
 const MainDrawer = createDrawerNavigator({
-    screenOptions:{
+    screenOptions:({route})=>{
+        return {
         drawerStyle:{
             width:'60%',
             backgroundColor:colors.WHITE,
@@ -26,6 +50,7 @@ const MainDrawer = createDrawerNavigator({
         drawerActiveBackgroundColor:colors.PINK_700,
         drawerInactiveTintColor:colors.GRAY_500,
         drawerInactiveBackgroundColor:colors.GRAY_100,
+        drawerIcon : ({focused})=> DrawerIcons(route.name as keyof MainDrawerParamList, focused),
         headerTitleAlign: 'center',
     headerBackButtonDisplayMode: 'minimal',
     headerTintColor: colors.BLACK,
@@ -36,6 +61,7 @@ const MainDrawer = createDrawerNavigator({
     headerTitleStyle: {
       fontSize: 16,
     },
+    }
     },
     screens : {
         Map : {
