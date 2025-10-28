5216753dbafcb10e1cd62dcb0fbd4742d4efc34c
soundpost1682 <guarneri1682@gmail.com>
2025-10-28 11:26:31 -0700

    General Button compo

--- src/screens/auth/AuthHomeScreen.tsx
+++ src/screens/auth/AuthHomeScreen.tsx
@@ -1,9 +1,9 @@
 import {useNavigation} from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
-import {SafeAreaView} from 'react-native-safe-area-context';
-import { AuthStackParamList } from '../../types/navigation';
+import {SafeAreaView, StyleSheet} from 'react-native';
+import { AuthStackParamList } from '@/types/navigation';
+import CustomButton from '@/components/CustomButton';
 
 type Navigation = StackNavigationProp<AuthStackParamList>
 
@@ -12,16 +12,14 @@ function AuthHomeScreen() {
 
   return (
     <SafeAreaView>
-      <Text
-        onPress={() => {
-          navigation.navigate('Login');
-        }}>
-        Go Log-In
-      </Text>
+      <CustomButton label='Email Login' onPress={()=>navigation.navigate('Login')}/>
+      
     </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  
+});
 
 export default AuthHomeScreen;
